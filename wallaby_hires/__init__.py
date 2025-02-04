26d3e2b1b60bb8852c60a0ced4ccaa5cbad0e8c9
Gayatri Aniruddha <gayatri.research21@gmail.com>
2025-02-04 14:44:40 +0800

    all functions publicly available

--- wallaby_hires/__init__.py
+++ wallaby_hires/__init__.py
@@ -1,7 +1,9 @@
 __package__ = "wallaby_hires"
 # The following imports are the binding to the DALiuGE system
 
-# extend the following as required
-from .funcs import parset_mixin
+# Import everything from funcs.py
+from .funcs import *
 
-__all__ = ["parset_mixin"]
+# extend the following as required
+# from .funcs import parset_mixin
+# __all__ = ["parset_mixin"]
